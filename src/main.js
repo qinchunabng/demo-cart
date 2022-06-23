a36b6a33dfe1c17cb3937cddc838312ce64d9507
qinchunabng <373413704@qq.com>
2022-06-23 13:06:57 +0800

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import App2 from '@/App2.vue'
+import router from '@/router'
 // 导入 bootstrap 样式表
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App2),
+  router
 }).$mount('#app')
